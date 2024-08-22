cc10f98eaf04c7b56b432ae86db1a1aeb2540553
anshshr <anshshrivastav24@gmail.com>
2024-08-22 12:12:40 +0530

    hide the api key

--- lib/gemini/secrets.dart
+++ lib/gemini/secrets.dart
@@ -1,5 +1,5 @@
-// final gemini_api_key_value = "AIzaSyDi4t3C0UUeZArtyQiAO6alqKgwYc5JHio";
-final gemini_api_key_value = "AIzaSyDl6sI44mSHuLr4cpZFOLTqulztUZSav-o";
+// final gemini_api_key_value = "your api key";
+final gemini_api_key_value = "your api key";
 
 final serp_api_key =
-    'ecb0f7cc1cbba52f415996748a48cbbefff008aa2e2daa696a64e143e240ec4c';
+    'your api key';
